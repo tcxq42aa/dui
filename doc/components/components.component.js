ae0321a1039267aeb056561657e4d8b124a68d0c
charles <xiaoaojianghuxq@126.com>
2016-08-05 15:35:13 +0800

    init website

--- doc/components/components.component.js
+++ doc/components/components.component.js
@@ -1,4 +1,9 @@
 "use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -12,19 +17,25 @@ var __metadata = (this && this.__metadata) || function (k, v) {
  * Created by charles on 16/8/3.
  */
 var core_1 = require("@angular/core");
-var ComponentsComponent = (function () {
+var base_component_1 = require("../base.component");
+var sidebar_directive_1 = require("../sidebar/sidebar.directive");
+var page_component_1 = require("../page/page.component");
+var ComponentsComponent = (function (_super) {
+    __extends(ComponentsComponent, _super);
     function ComponentsComponent() {
+        _super.apply(this, arguments);
     }
     ComponentsComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'page-components',
             templateUrl: 'components.component.html',
-            styleUrls: ['components.component.css']
+            styleUrls: ['components.component.css'],
+            directives: [page_component_1.PageComponent, sidebar_directive_1.SidebarDirective]
         }), 
         __metadata('design:paramtypes', [])
     ], ComponentsComponent);
     return ComponentsComponent;
-}());
+}(base_component_1.BaseComponent));
 exports.ComponentsComponent = ComponentsComponent;
 //# sourceMappingURL=components.component.js.map
--- doc/components/components.component.ts
+++ doc/components/components.component.ts
@@ -2,12 +2,16 @@
  * Created by charles on 16/8/3.
  */
 import {Component} from "@angular/core";
+import {BaseComponent} from "../base.component";
+import {SidebarDirective} from "../sidebar/sidebar.directive";
+import {PageComponent} from "../page/page.component";
 @Component({
     moduleId: module.id,
     selector: 'page-components',
     templateUrl: 'components.component.html',
-    styleUrls: ['components.component.css']
+    styleUrls: ['components.component.css'],
+    directives: [PageComponent, SidebarDirective]
 })
-export class ComponentsComponent {
+export class ComponentsComponent extends BaseComponent{
 
 }
